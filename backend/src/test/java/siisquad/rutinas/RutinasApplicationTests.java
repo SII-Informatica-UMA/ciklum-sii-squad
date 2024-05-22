48c0e5568db489246fdf1c89bc98dcb218be4d31
DjRl0 <061089651x@uma.es>
2024-05-22 09:02:01 +0200

    actualizar propiedades

--- backend/src/test/java/siisquad/rutinas/RutinasApplicationTests.java
+++ backend/src/test/java/siisquad/rutinas/RutinasApplicationTests.java
@@ -11,22 +11,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
-import org.springframework.web.util.DefaultUriBuilderFactory;
-import org.springframework.web.util.UriBuilder;
-import org.springframework.web.util.UriBuilderFactory;
-
+import org.springframework.test.context.TestPropertySource;
 
 import siisquad.rutinas.entities.*;
 import siisquad.rutinas.repositories.*;
 import siisquad.rutinas.dtos.*;
 
-@PropertySource("classpath:application-test.properties")
+@TestPropertySource(locations = "classpath:application-test.properties")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DisplayName("Tests api Rest")
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
