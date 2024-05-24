ef6c772e5c6596814bc08695ce0787dea0edd268
DjRl0 <061089651x@uma.es>
2024-05-24 10:59:25 +0200

    mockito funciona

--- backend/src/main/java/siisquad/rutinas/servicios/GestionEntrenamientoService.java
+++ backend/src/main/java/siisquad/rutinas/servicios/GestionEntrenamientoService.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Service
 public class GestionEntrenamientoService {
     @Bean
-    public RestTemplate restTemplate() {
+    public static RestTemplate restTemplate() {
         return new RestTemplate();
     }
     @Autowired
